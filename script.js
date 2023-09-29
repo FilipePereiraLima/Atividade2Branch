82717858221a00a5174862f1428fa3dbe7754bc7
Caio H Perlin <caiohperlin@gmail.com>
2023-09-28 23:51:23 -0400

    feat: implement confetti

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-function funcaobutton()
-{
-alert("DONT PRESS ME! PLEASE🐱‍👤");
+function funcaobutton(){
+    alert("DONT PRESS ME! PLEASE🐱‍👤");
 }
+
+const jsConfetti = new JSConfetti();
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('confettiButton').addEventListener('click', () => jsConfetti.addConfetti({
+        emojis: ['🚶', '⛏']
+    }));
+});
